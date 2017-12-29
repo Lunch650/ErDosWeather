2f78700a48bd599afccb65f82d7e8260934fac75
lunch650 <lunch650@qq.com>
2017-12-29 12:10:38 +0800

    第一版基本完成

--- ErDosWeather.py
+++ ErDosWeather.py
@@ -6,11 +6,14 @@ from bs4 import BeautifulSoup
 
 
 def pagecontent(code):
-    # 获取页面数据 TODO 需要catch
+    # 获取页面数据
     url = 'http://wisdom.tqonline.top/weiqixiang/tianqi/getforecastbystations?stations=[' + code + ']'
-    r = requests.get(url)
-    weathersoup = BeautifulSoup(r.content, "html.parser")
-    return weathersoup.body
+    try:
+        r = requests.get(url, timeout=10)
+        weathersoup = BeautifulSoup(r.content, "html.parser")
+        return weathersoup.body
+    except requests.Timeout:
+        print('呀，网络好像出现了一些问题，重新运行一次吧')
 
 
 def pagelines(code):
@@ -28,13 +31,13 @@ def weatherlines(prelist):
         print('报歉，' + str(prelist[0])[:-1] + '暂无预报数据。')
         return []
     elif prelist.count('报歉，暂无预报数据。') == 1:
-        weatherweek = [d[d.index('：') + 1:] for d in prelist[1:prelist[1:].index(prelist[0])+1]]
+        weatherweek = [[d[d.index('：') + 1:] for d in prelist[1:prelist[1:].index(prelist[0])+1]], ]
         return weatherweek
     else:
         weatherweek = \
             [
                 [
-                    d[d.index('：') + 1:] for d in prelist[1:prelist[1:].index(prelist[0])+1]
+                    d[d.index('：') + 1:] for d in prelist[1:prelist[1:].index(prelist[0]) + 1]
                 ],
                 [
                     s[s.index('：') + 1:] for s in prelist[prelist[1:].index(prelist[0]) + 2:]
@@ -43,16 +46,16 @@ def weatherlines(prelist):
         return weatherweek
 
 
-def weatherdocx(weatherweek):
-    d = Document('weatherTemplate.docx')
-    t = d.tables[0]
+def weatherdocx(weatherweek, docname, startrow):
+    doc = Document(docname)
+    t = doc.tables[0]
     for column, weatherday in enumerate(weatherweek):
         templist = weatherday.split('，')
         temperature = templist[2][4:-1] + '~' + templist[3][4:-2]
         weather = [templist[0], temperature, templist[1]]
         for row, elementinday in enumerate(weather):
-            t.cell(row + 1, column + 3).text = elementinday
-    d.save('bbb.docx')
+            t.cell((startrow * 3) + row + 1, column + 3).text = elementinday
+    doc.save(docname)
 
 
 def predocx():
@@ -60,18 +63,15 @@ def predocx():
     d = Document('weatherTemplate.docx')
     d.paragraphs[1].text = '(' + dt.strftime('%Y') + '年第' + dt.strftime('%W') + '期）'
     d.paragraphs[3].text = dt.strftime('%Y') + '年' + dt.strftime('%m') + '月' + dt.strftime('%d') + '日'
-    print(d.paragraphs[1].text)
     t = d.tables[0]
     for i in range(1, 8):
         dttemp = dt + timedelta(days=i)
         t.cell(0, i + 2).text = (dttemp.strftime('%m') + '月' + dttemp.strftime('%d') + '日')
-        # TODO 需要对内容格式作处理
     d.save(dt.strftime('%Y%m%d') + '08.docx')
     d.save(dt.strftime('%Y%m%d') + '16.docx')
 
 
 if __name__ == '__main__':
-
     # citydict = {
     #     '准格尔旗': '53553',
     #     '清水河县': '53562',
@@ -80,21 +80,30 @@ if __name__ == '__main__':
     #     '丰镇': '53484',
     #     '大同': '53487',
     #     '秦皇岛': '54449',
+    #     '和林格尔县': '53469',
+    #     '凉城': '53475',
     #     '右玉': '53478',
     #     '平鲁': '53574',
     #     '朔州': '53578',
     #     '神池': '53575',
     # }
     dt = datetime.now()
-    citylist = ['53553', '53562', '53469', '53475', '53484', '53487', '54449', '53478', '53574', '53578', '53575', ]
+    citylist = \
+        [
+            '53553', '53562', '53469', '53475', '53484', '53487', '54449',
+            '53469', '53475', '53478', '53574', '53578', '53575',
+        ]
     predocx()
     for index, citycode in enumerate(citylist):
-        weatherweek = weatherlines(pagelines(citycode))
-        if len(weatherweek) == 0:
-            d8 = Document(dt.strftime('%Y%m%d') + '08.docx')
-            print(d8.paragraphs[3].text)
-            d16 = Document(dt.strftime('%Y%m%d') + '16.docx')
-            print(d16.paragraphs[3].text)
+        weatherweeks = weatherlines(pagelines(citycode))
+        if len(weatherweeks) > 0:
+            # 如果页面有数据则先做出8点版本
+            doc8name = dt.strftime('%Y%m%d') + '08.docx'
+            weatherdocx(weatherweeks[0], doc8name, index)
+            if len(weatherweeks) > 1:
+                # 如果页面有8点及16点版本则更新
+                d16 = Document(dt.strftime('%Y%m%d') + '16.docx')
+                weatherdocx(weatherweeks[1], d16, index)
     # for (cityname, citycode) in cityList.items():
     #     weatherlist = weatherlines(citycode)
     # weatherlineone = weatherlines[:int(len(weatherlines)/2)]

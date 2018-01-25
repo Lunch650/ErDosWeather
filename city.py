d9a85be6bb636ef9c91cf0f36d00f7f2b061e225
lunch650 <lunch650@qq.com>
2018-01-25 15:32:50 +0800

    完成

--- city.py
+++ city.py
@@ -31,6 +31,7 @@ class City(object):
                 print(self.__name + ':页面加载连接超时，加载任务重新启动中')
         return BeautifulSoup(r.content, 'html.parser').body
 
+    @staticmethod
     def weekly_weather(mess_str):
         # 清洗页面返回的字符串内容,并根据内容将返回内容分开为两个list，返回内容格式为list
         if mess_str is not '':
@@ -97,39 +98,3 @@ class City(object):
             t.cell(0, column + 2).text = (dttemp.strftime('%m') + '月' + dttemp.strftime('%d') + '日')
         for doc_name in City.doc_names:
             d.save(doc_name)
-
-
-if __name__ == '__main__':
-    def __main():
-        cities = {
-            '53553': '准格尔旗',
-            '53562': '清水河县',
-            '53484': '丰镇',
-            '53487': '大同',
-            '54449': '秦皇岛',
-            '53469': '和林格尔县',
-            '53475': '凉城',
-            '53478': '右玉',
-            '53574': '平鲁',
-            '53578': '朔州',
-            '53575': '神池',
-        }
-        cities_order = \
-            [
-                '53553', '53562', '53469', '53475', '53484', '53487', '54449',
-                '53469', '53475', '53478', '53574', '53578', '53575',
-            ]
-
-        City.predocx()
-        print('今日数据模板准备完毕')
-        for i, code in enumerate(cities_order):
-            city = City(cities.get(code), code)
-            weather = City.weekly_weather(city.content_from_page())
-            if len(weather) > 0:
-                City.save_doc(doc_name=City.doc_names[0], weekly_weather=weather[0], start_row=i)
-                print(cities.get(code), '8点版本写入完成')
-                if len(weather) > 1:
-                    City.save_doc(doc_name=City.doc_names[1], weekly_weather=weather[1], start_row=i)
-                    print(cities.get(code), '20点版本写入完成')
-
-    __main()
